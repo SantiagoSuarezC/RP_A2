2ea6c6dbbe093027df147e7dbccaeca724badd06
Nando Nunez <nandonunezsanchez@gmail.com>
2023-11-21 16:44:49 +0100

    Limpeza

--- show_batch_txt.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-for %%f in (.\input\*.txt) do (
-    python encode_original.py < %%f > output\original\domain_original_%%~nf.lp
-    clingo 0 lightup.lp output\original\domain_original_%%~nf.lp --outf=2 | python decode.py > output\original\txt\solution_%%~nf.txt
-)
-
-endlocal
