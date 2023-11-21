6fb25a92c637991fe54bdd3aef1802ad189189b5
Santiago Suárez <110847132+SantiagoSuarezC@users.noreply.github.com>
2023-11-21 19:45:20 +0100

    Final changes

---
+++ RP_LabAssignment2/encode_modified.py
@@ -0,0 +1,13 @@
+import sys
+
+lineas = list(sys.stdin)
+total_de_filas = len(lineas)
+
+for i, line in enumerate(lineas):
+    j = total_de_filas - i - 1
+    for k, c in enumerate(line.strip()):
+        if c in ['x', 'b', '0', '1', '2', '3', '4']:
+            print(f"cell({k},{j},{c}).")
+
+print(f"dim({total_de_filas}).")
+print(f"#const n={total_de_filas}.")
